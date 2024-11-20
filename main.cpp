f4b76fafaf435bc451e9a4d907baa7213f69c159
16yunH <hy20051123@gmail.com>
2024-11-20 14:25:23 +0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <vector>
 #include "lib/tinyxml2.h"
@@ -7,5 +8,32 @@ using namespace std;
 
 
 int main() {
+    return 0;
+}
+*/
+
+#include <iostream>
+#include "src/OsmParser.h"
+
+int main() {
+    OsmParser parser("D:/3.code/CLionProjects/MapNavigation/data/map.osm");
+
+    if (parser.parse()) {
+        const std::vector<Node>& nodes = parser.getNodes();
+        const std::vector<Way>& ways = parser.getWays();
+
+        std::cout << "Nodes: " << nodes.size() << std::endl;
+        std::cout << "Ways: " << ways.size() << std::endl;
+
+        // 输出部分解析的节点和路径信息
+        for (const auto& node : nodes) {
+            std::cout << "Node ID: " << node.id << ", Lat: " << node.lat << ", Lon: " << node.lon << std::endl;
+        }
+
+        for (const auto& way : ways) {
+            std::cout << "Way ID: " << way.id << ", Nodes: " << way.nodeIds.size() << std::endl;
+        }
+    }
+
     return 0;
 }
