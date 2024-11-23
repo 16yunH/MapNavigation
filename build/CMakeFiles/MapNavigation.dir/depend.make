8d463780b9eb2a0f9eac261cec39a8761b33a2c5
16yunH <hy20051123@gmail.com>
2024-11-23 15:54:29 +0800

    Initial commit

--- build/CMakeFiles/MapNavigation.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for MapNavigation.
-# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 
 #include "src/PathFinder.h"
 #include "src/OsmParser.h"
-#include "lib/httplib.h"
 #include "lib/tinyxml2.h"
-#include <iostream>
-
-/*
 #include <iostream>
 #include <vector>
-#include "lib/tinyxml2.h"
+
+/*
 
 using namespace tinyxml2;
 using namespace std;
@@ -22,8 +19,6 @@ int main() {
 
 //example code for Praser
 /*
-#include <iostream>
-#include "src/OsmParser.h"
 
 int main() {
     OsmParser parser("D:/3.code/CLionProjects/MapNavigation/data/mapofgx.osm");
